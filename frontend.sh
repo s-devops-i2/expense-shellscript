03962cf7056966959d5621fd04063b3c919482f7
shuja <shuja.hafez@gmail.com>
2024-10-09 17:02:04 +0530

    print function

--- frontend.sh
+++ frontend.sh
@@ -4,28 +4,28 @@ print_heading "Install Nginx"
 echo $?
 dnf install nginx -y &>>/tmp/data.log
 echo $?
-echo Remove old content
+print_heading  Remove old content
 rm -rf /usr/share/nginx/html/*
 echo $?
 
-echo Create Nginx Reverse Proxy Configuration.
+print_heading  Create Nginx Reverse Proxy Configuration.
 cp expense.conf /etc/nginx/default.d/expense.conf
 echo $?
 
-echo Download frontend content
+print_heading  Download frontend content
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>/tmp/data.log
 echo $?
 
-echo Extract the frontend content.
+print_heading  Extract the frontend content.
 cd /usr/share/nginx/html &>>/tmp/data.log
 echo $?
 
 unzip /tmp/frontend.zip &>>/tmp/data.log
 echo $?
 
-echo Enable nginx
+print_heading  Enable nginx
 systemctl enable nginx &>>/tmp/data.log
 echo $?
-echo Start nginx
+print_heading  Start nginx
 systemctl start nginx
 echo $?
