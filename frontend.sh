3b9ead21bdb3c0b805cf11f49980dc15bf81f8dd
shuja <shuja.hafez@gmail.com>
2024-10-09 11:09:02 +0530

    frontend shellscript

---
+++ frontend.sh
@@ -0,0 +1,22 @@
+
+echo Install Nginx
+
+dnf install nginx -y &>>tmp.log
+
+echo Remove old content
+rm -rf /usr/share/nginx/html/*
+
+echo Download frontend content
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>tmp.log
+
+echo Extract the frontend content.
+cd /usr/share/nginx/html
+
+unzip /tmp/frontend.zip
+
+echo Create Nginx Reverse Proxy Configuration.
+
+cp expense.conf /etc/nginx/default.d/expense.conf
+
+systemctl enable nginx
+systemctl restart nginx
