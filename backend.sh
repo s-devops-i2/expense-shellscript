b891788f9ed5973debb3854a8384fd11d6106d81
shuja <shuja.hafez@gmail.com>
2024-10-09 17:08:57 +0530

    print function

--- backend.sh
+++ backend.sh
@@ -1,31 +1,33 @@
-echo disable nodejs
+source common.sh
+
+print_heading "disable nodejs"
 dnf module disable nodejs -y &>>/tmp/data.log
 echo $?
-echo enable nodejs
+print_heading  "enable nodejs"
 dnf module enable nodejs:20 -y &>>/tmp/data.log
 echo $?
 
-echo install nodejs
+print_heading  "install nodejs"
 dnf install nodejs -y &>>/tmp/data.log
 echo $?
 
 
-echo Add application User
+print_heading  "Add application User"
 id expense &>>/tmp/data.log
 if [ $? -ne 0 ]; then
 useradd expense
 fi
 
-echo setup an app directory.
+print_heading  "setup an app directory."
 if [ ! -d /app  ]; then
 mkdir /app
 fi
 
 
-echo copy Backend Service
+print_heading  "copy Backend Service"
 cp backend.service /etc/systemd/system/backend.service
 
-echo Download the application code to created app directory.
+print_heading  "Download the application code to created app directory."
 
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/data.log
 echo $?
@@ -36,27 +38,27 @@ unzip /tmp/backend.zip &>>/tmp/data.log
 fi
 echo $?
 
-echo download the dependencies.
+print_heading  "download the dependencies."
 
 cd /app
 npm install &>>/tmp/data.log
 echo $?
 
-echo Load the service.
+print_heading  "Load the service."
 
 systemctl daemon-reload
 echo $?
-echo enable the service.
+print_heading  "enable the service."
 
 systemctl enable backend &>>/tmp/data.log
 echo $?
-echo Start the service.
+print_heading  "Start the service."
 systemctl start backend
 echo $?
-echo install mysql client
+print_heading  "install mysql client"
 dnf install mysql -y &>>/tmp/data.log
 echo $?
-echo Load Schema
+print_heading  "Load Schema"
 
 mysql -h 172.31.31.229 -uroot -pExpenseApp@1 < /app/schema/backend.sql
 echo $?
--- mysql.sh
+++ mysql.sh
@@ -1,14 +1,16 @@
-echo Install MySQL Server 8.0.x
+source common.sh
+
+print_heading  "Install MySQL Server 8.0.x"
 
 dnf install mysql-server -y &>>/tmp/data.log
 echo $?
-echo Start MySQL Service
+print_heading  "Start MySQL Service"
 
 systemctl enable mysqld &>>/tmp/data.log
 echo $?
 systemctl start mysqld
 echo $?
-echo Next, We need to change the default root password in order to start using the database service. Use password ExpenseApp@1 or any other as per your choice.
+print_heading "Change the default root password"
 
 mysql_secure_installation --set-root-pass ExpenseApp@1
 echo $?
