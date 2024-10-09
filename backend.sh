54c6af8ced620a2bb4d0771632d50cd9f1850967
shuja <shuja.hafez@gmail.com>
2024-10-09 14:45:39 +0530

    exit status added

--- backend.sh
+++ backend.sh
@@ -1,51 +1,56 @@
 echo disable nodejs
 dnf module disable nodejs -y &>>/tmp/data.log
+echo $?
 echo enable nodejs
 dnf module enable nodejs:20 -y &>>/tmp/data.log
+echo $?
 
 echo install nodejs
 dnf install nodejs -y &>>/tmp/data.log
+echo $?
 
 
 echo Add application User
 
 useradd expense
-
-echo Lets setup an app directory.
+echo $?
+echo setup an app directory.
 
 mkdir /app
+
+echo Setup SystemD Expense Backend Service
+cp backend.service /etc/systemd/system/backend.service
 
 echo Download the application code to created app directory.
 
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/data.log
+echo $?
 cd /app
 unzip /tmp/backend.zip &>>/tmp/data.log
+echo $?
 
-echo Setup SystemD Expense Backend Service
-
-cp backend.service /etc/systemd/system/backend.service
 
 echo download the dependencies.
 
 #cd /app
 npm install &>>/tmp/data.log
-
+echo $?
 
 echo Load the service.
 
 systemctl daemon-reload
-
+echo $?
 echo enable the service.
 
 systemctl enable backend &>>/tmp/data.log
-
+echo $?
 echo Start the service.
 systemctl start backend
-
+echo $?
 echo install mysql client
 dnf install mysql -y &>>/tmp/data.log
-
+echo $?
 echo Load Schema
 
 mysql -h 172.31.31.229 -uroot -pExpenseApp@1 < /app/schema/backend.sql
-
+echo $?
--- mysql.sh
+++ mysql.sh
@@ -1,13 +1,14 @@
 echo Install MySQL Server 8.0.x
 
 dnf install mysql-server -y &>>/tmp/data.log
-
+echo $?
 echo Start MySQL Service
 
 systemctl enable mysqld &>>/tmp/data.log
+echo $?
 systemctl start mysqld
-
+echo $?
 echo Next, We need to change the default root password in order to start using the database service. Use password ExpenseApp@1 or any other as per your choice.
 
 mysql_secure_installation --set-root-pass ExpenseApp@1
-
+echo $?
