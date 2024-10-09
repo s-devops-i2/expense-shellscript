e36ebf1f6ae0ffe4fb66df5c2ad963d1e5c41085
shuja <shuja.hafez@gmail.com>
2024-10-09 11:19:15 +0530

    changed the order of cp command

--- frontend.sh
+++ frontend.sh
@@ -6,6 +6,9 @@ dnf install nginx -y &>>tmp.log
 echo Remove old content
 rm -rf /usr/share/nginx/html/*
 
+echo Create Nginx Reverse Proxy Configuration.
+cp expense.conf /etc/nginx/default.d/expense.conf
+
 echo Download frontend content
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>tmp.log
 
@@ -13,10 +16,6 @@ echo Extract the frontend content.
 cd /usr/share/nginx/html
 
 unzip /tmp/frontend.zip
-
-echo Create Nginx Reverse Proxy Configuration.
-
-cp expense.conf /etc/nginx/default.d/expense.conf
 
 systemctl enable nginx
 systemctl restart nginx
